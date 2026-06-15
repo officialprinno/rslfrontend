30048b59f83119c793661fb81b0ee748a9bfb9f8
PrinnoTech198 <innocent.pachalio@gmail.com>
2026-06-15 15:05:17 +0300

    production root

--- src/app/environments/environments.prod.ts
+++ src/app/environments/environments.prod.ts
@@ -1,15 +1,5 @@
-// export const environment = {
-//     production: true,
-//     apiUrl: 'https://api.rocksolutions.co.tz/api/v1',
-//     wsUrl: 'wss://api.rocksolutions.co.tz/ws'
-// };
-
 export const environment = {
-    production: true,
-
-    apiUrl:
-        'https://rslbackend-production.up.railway.app/api/v1',
-
-    wsUrl:
-        'wss://rslbackend-production.up.railway.app/ws'
+  production: true,
+  apiUrl: 'https://web-production-84bc8e.up.railway.app/api/v1',
+  wsUrl: 'wss://web-production-84bc8e.up.railway.app/ws',
 };
